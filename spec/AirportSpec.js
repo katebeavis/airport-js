30ed9f1e6f594c9af1032d8805fbd7d852b75bca
gabe <GabeMaker@users.noreply.github.com>
2015-03-03 18:39:42 +0000

    airport knows planes have taken off

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,21 +1,35 @@
 describe ("Airport", function() {
 
+  var airport;
+
+  beforeEach(function() {
+    airport = new Airport;
+    plane = jasmine.createSpyObj('plane',['land','takeOff'])
+  });
+
   it ("should be empty when created", function() {
     expect(new Airport().planes).toEqual ([])
   });
 
   it ("should tell plane to land", function() {
-    airport = new Airport
-    plane = jasmine.createSpyObj('plane',['land'])
     airport.land(plane);
     expect(plane.land).toHaveBeenCalled();
   });
 
   it ("should know a plane has landed", function() {
-  	airport = new Airport
-    plane = jasmine.createSpyObj('plane',['land'])
   	airport.land(plane);
   	expect(airport.planes).toEqual([plane]);
+  });
+
+  it ("should be able to tell a plane to take off", function() {
+    airport.takeOff(plane);
+    expect(plane.takeOff).toHaveBeenCalled();
+  });
+
+  it ("should know a plane has taken off", function() {
+    airport.land(plane);
+    airport.takeOff(plane);
+    expect(airport.planes).toEqual ([]);
   });
 
 });
--- src/Airport.js
+++ src/Airport.js
@@ -6,3 +6,8 @@ Airport.prototype.land = function(plane) {
 	plane.land();
 	this.planes.push(plane);
 };
+
+Airport.prototype.takeOff = function(plane) {
+  plane.takeOff();
+  this.planes.pop(plane);
+};
